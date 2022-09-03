74756ed618429303bb9b67aecf78f39974af9a37
andersonlima013 <andersonwow302@gmail.com>
2022-09-03 17:41:26 -0300

    adicionando a lista suspensa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
+
 function App() {
   return (
     <div className="App">
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,15 @@
 import './index.css'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 
 const Formulario = () => {
+
+    const times = [
+        'Projetos',
+        'Cadastro',
+        'Orçamento',
+        'PCP'
+    ]
     return (
         <section className="formulario">
             <form>
@@ -9,6 +17,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome =)" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times} placeholder="Escolha o seu time" />
             </form>
         </section>
     )
